6ffa5d41491e0d84e2bd9aa5f3f1f598cc00ae29
Srijan Agarwal <srijanagarwal.cse@gmail.com>
2017-06-14 19:34:00 +0530

    Added ot.js : Wrote preconditions for taking input and preconditions for operations and transform functions

---
+++ ot.js
@@ -0,0 +1,29 @@
+//A naive implementation of operational transformation
+
+//NOTE : All operations have properties as 'pos'(which means position), 'ty'(which means type i.e whether insert or delete  and 'id' 
+//For eg. {ty: 'ins', pos: 2, ch: 'x', pri: 0} or {ty: 'del', pos: 2}
+
+//Every character 'i' is taken as input into the tree data structure
+//Precondition for taking input is that 'i' may not be a member of the set
+
+function sizeOf(tree) {
+	return tree == null ? 0 : tree.size;
+}
+
+function pos(tree, i) {
+	var base = 0;
+	while (tree != null) {
+		var left = tree.left;
+		var p = tree.value - sizeOf(left);
+		if (i < p) {
+			tree = left;
+		} 
+		else {
+			i = 1 + i - p;
+			base = base + tree.value;
+			tree = tree.right;
+		}
+	}
+	return base + i;
+}
+
